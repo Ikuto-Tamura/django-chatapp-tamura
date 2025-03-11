fa82c2773a3573adc05c4fd1a36c7218d0c0a904
Tamm2 <totukakidikuto2525@gmail.com>
2025-03-12 04:15:36 +0900

    手順書作成

--- chatapp_pj/chatapp_app/models.py
+++ chatapp_pj/chatapp_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from django.contrib.auth.models import AbstractUser
- 
+
 class User(AbstractUser):
     user_icon = models.ImageField('プロフィール画像',upload_to='user_icons',default="sori_snow_boy.png")
 
