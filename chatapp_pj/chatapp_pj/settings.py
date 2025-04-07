a17eacce474018c81e563e676a33f1639b428107
Tamm2 <totukakidikuto2525@gmail.com>
2025-04-08 06:17:31 +0900

    cssはねい2

--- chatapp_pj/chatapp_pj/settings.py
+++ chatapp_pj/chatapp_pj/settings.py
@@ -132,9 +132,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-STATICFILES_URL = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "chatapp_app/static"),
 ]
+
+STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
