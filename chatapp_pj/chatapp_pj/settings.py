e2aa1d238faebb04d86dc8b3bedbf7f82122a9e9
Tamm2 <totukakidikuto2525@gmail.com>
2025-04-08 06:26:43 +0900

    パスを正しく

--- chatapp_pj/chatapp_pj/settings.py
+++ chatapp_pj/chatapp_pj/settings.py
@@ -132,13 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "chatapp_app/static"),
-]
-
-STATIC_URL = "static/"
-
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "chatapp_app", "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
