01f6f0f38c2cf6adef3ad0af18b39008dd79ca5f
Tamm2 <totukakidikuto2525@gmail.com>
2025-04-08 07:31:28 +0900

    staticfilesストレージ

--- chatapp_pj/chatapp_pj/settings.py
+++ chatapp_pj/chatapp_pj/settings.py
@@ -135,6 +135,11 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+if not DEBUG:
+    STATICFILES_STORAGE = (
+        "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+    )
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
