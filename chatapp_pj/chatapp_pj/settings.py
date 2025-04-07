18e71d04e99fdf0a787ef5a82f4dd19faf5326c2
Tamm2 <totukakidikuto2525@gmail.com>
2025-04-08 06:21:54 +0900

    cssはねい3

--- chatapp_pj/chatapp_pj/settings.py
+++ chatapp_pj/chatapp_pj/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "chatapp_app/static"),
 ]
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
